765db8fd003c8cb407e8c4f52da32fd6d0f40f40
Timothy Do <timothydobsa@gmail.com>
2020-09-27 19:04:04 -0700

    no need for this

--- pifmmorse.py
+++
@@ -1,7 +0,0 @@
-#Author: Timothy Do
-import os
-
-print("Welcome to PiFmMorse for Python 3!\n")
-print("*******Compiling the Fm Transmitter Executable*******")
-os.system("make")
-print("*******Finish Compiling the Fm Transmitter Executable*******\n")
