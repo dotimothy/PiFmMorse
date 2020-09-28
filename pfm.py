72418394f5b0222d6375da9480e3c3ee8aeabcf3
Timothy Do <timothydobsa@gmail.com>
2020-09-27 19:08:09 -0700

    Create pfm.py

---
+++ pfm.py
@@ -0,0 +1,7 @@
+#Author: Timothy Do
+import os
+
+print("Welcome to PiFmMorse for Python 3!\n")
+print("*******Compiling the FM Transmitter Executable*******\n")
+os.system("make")
+print("\n*******Finish Compiling the Fm Transmitter Executable*******\n")
