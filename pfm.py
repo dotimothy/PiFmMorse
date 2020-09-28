e05b6376f4739751b441d9e088629af263b9bd93
dotimothy <timothydobsa@gmail.com>
2020-09-27 19:02:59 -0700

    rename

---
+++ pfm.py
@@ -0,0 +1,7 @@
+#Author: Timothy Do
+import os
+
+print("Welcome to PiFmMorse for Python 3!\n")
+print("*******Compiling the Fm Transmitter Executable*******")
+os.system("make")
+print("*******Finish Compiling the Fm Transmitter Executable*******\n")
