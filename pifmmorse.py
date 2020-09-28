31a615b6bb721bee1a62ca4c70053a736313631d
Timothy Do <timothydobsa@gmail.com>
2020-09-27 18:54:50 -0700

    Update pifmmorse.py

--- pifmmorse.py
+++ pifmmorse.py
@@ -1,3 +1,7 @@
+#Author: Timothy Do
 import os
 
+print("Welcome to PiFmMorse for Python 3!")
+print("*******Compiling the Fm Transmitter Executable*******")
 os.system("make")
+print("*******Finish Compiling the Fm Transmitter Executable*******")
