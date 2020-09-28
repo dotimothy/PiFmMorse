4842e4076d9b2fcafb03db8564b5cb425618f47c
Timothy Do <timothydobsa@gmail.com>
2020-09-27 21:44:01 -0700

    Update pfm.py

--- pfm.py
+++ pfm.py
@@ -9,5 +9,5 @@ import os
 
 print("Welcome to PiFmMorse for Python 3!\n")
 print("*******Compiling the FM Transmitter Executable*******\n")
-os.system("make")
+os.system("sudo make")
 print("\n*******Finish Compiling the FM Transmitter Executable*******\n")
