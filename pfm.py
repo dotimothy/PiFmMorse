3003fd0ec16393c6ffe1721fd335e2367cc02da4
Timothy Do <timothydobsa@gmail.com>
2020-09-27 21:56:22 -0700

    Update pfm.py

--- pfm.py
+++ pfm.py
@@ -1,7 +1,17 @@
 #Author: Timothy Do
+try: 
+	import RPi.GPIO
+except ModuleNotFoundError:
+	print("You Don't Have GPIO Pins. Terminating...")
+	exit()
+
 import os
+import wave
 
 print("Welcome to PiFmMorse for Python 3!\n")
-print("*******Compiling the Fm Transmitter Executable*******\n")
+print("*******Compiling the FM Transmitter Executable*******\n")
 os.system("sudo make")
-print("\n*******Finish Compiling the Fm Transmitter Executable*******\n")
+print("\n*******Finish Compiling the FM Transmitter Executable*******\n")
+
+sequence = []
+frames = []
