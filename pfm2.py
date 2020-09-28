7f67d5403ffbb52cf3db94314d38675bdca57f6f
Timothy Do <timothydobsa@gmail.com>
2020-09-28 12:26:00 -0700

    Update pfm2.py

--- pfm2.py
+++ pfm2.py
@@ -497,7 +497,7 @@ def prompt():
     print("Console Output:")
     printMorse(feed)
     compileOutput()
-    frequency = raw_input("\n\nThe output audio has been compiled. Specify the frequency to broadcast your output: ")
+    frequency = float(raw_input("\n\nThe output audio has been compiled. Specify the frequency to broadcast your output: "))
     print("\nBroadcasting your output at " + str(frequency) + " mhz!\n")
     os.system("sudo ./fm -f " + str(frequency) + " output.wav")
     print("\nThe Output has been Broadcasted. Thank you for using PiFmMorse!")
